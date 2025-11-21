d433ea2a69aa3b483c121598c45b57546309321e
Andrey Yaromenok <ayaromenok@gmail.com>
2025-11-21 15:26:33 +0200

    up: added Load Current/Power/% from UPS power

--- src/py/main.py
+++ src/py/main.py
@@ -48,26 +48,24 @@ def read_serial_data_EP20(ups_data, port_name):
 
                 old_ups_data = ups_data;
                 ups_data = struct.unpack(format_string1, data1)
-                if (ups_data[20] == 0) & (ups_data[0] != 1):
+                if (ups_data[20] == 0) & (ups_data[0] != 1): #sometime all zeros came from port
                     print ("use old values")
                     ups_data = old_ups_data;
 
-                gridVoltage = ups_data[7]*0.1; #V
-                gridFreq = ups_data[8]*0.1;    #Hz
-                outVoltage = ups_data[9]*0.1;  #V
-                outFreq = ups_data[10]*0.1;    #Hz
-                print(f"Work State:       {ups_data[4]}")
-                print(f"Battery Class:    {ups_data[5]} V")
-                print(f"Rated Power:      {ups_data[6]} W")
-    #            print(f"Grid Voltage: {gridVoltage:.1f} Volt {hex(ups_data[7])}") #debug example with hex
+#                if (ups_data[17] > 300): # when battery discarged - value 65468 appear isntead of 0#
+#                    ups_data[17] = 0;
+#                print(f"Work State:       {ups_data[4]}")
+#                print(f"Battery Class:    {ups_data[5]} V")
+#                print(f"Rated Power:      {ups_data[6]} W")
                 print(f"Grid Voltage:     {(ups_data[7]*0.1):.1f} V")
                 print(f"Grid Frequence:   {(ups_data[8]*0.1):.1f} Hz")
-                print(f"Outзut Voltage:   {(ups_data[9]*0.1):.1f} V")
+                print(f"Output Voltage:   {(ups_data[9]*0.1):.1f} V")
                 print(f"Output Frequency: {(ups_data[10]*0.1):.1f} Hz")
-
-                print(f"Battery Voltage:: {ups_data[16]*.1:.1f} V")
-                print(f"Battery Current:  {ups_data[17]*.1:.1f} A")
-
+                print(f"Battery Voltage:  {ups_data[16]*.1:.1f} V")
+                print(f"Bat Charge Curr:  {ups_data[17]*.1:.1f} A")
+                print(f"Bat Load Currrnt: {ups_data[11]} A")
+                print(f"Load Power:       {ups_data[12]} W")
+                print(f"Load Percent:     {ups_data[14]} %")
                 print(f"Battery SOC:      {ups_data[19]} %")
                 print(f"Transform Temp:   {ups_data[20]} C")
 
@@ -97,8 +95,11 @@ def publish_message(port_name, ups_data, time_interval):
         ups_data = read_serial_data_EP20(ups_data, port_name)
 #ToDo: don't send 0000000(zeros)
         client.publish("EP20/battery_voltage", round(ups_data[16]*0.1,1))
-        client.publish("EP20/battery_current", round(ups_data[17]*0.1,1))
+        client.publish("EP20/battery_charge_current", round(ups_data[17]*0.1,1))
+        client.publish("EP20/battery_load_current", round(ups_data[11]))
         client.publish("EP20/battery_SOC", ups_data[19])
+        client.publish("EP20/load_current", ups_data[11])
+        client.publish("EP20/load_power", ups_data[12])
         client.publish("EP20/transformer_temp", ups_data[20])
         client.publish("EP20/grid_voltage", round(ups_data[7]*0.1,1))
         client.publish("EP20/grid_frequency",round( ups_data[8]*0.1,1))
@@ -110,7 +111,7 @@ def publish_message(port_name, ups_data, time_interval):
     client.disconnect()
 
 if __name__ == "__main__":
-    time_interval = 10; #sec, for debugging
+    time_interval = 5; #sec, for debugging
     ups_data = init_all();
     print (ups_data);
     publish_message('/dev/ttyUSB0',ups_data, time_interval)
